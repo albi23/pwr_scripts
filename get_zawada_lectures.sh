227207b768c84d57eedf8af1098a69cda51b40c8
albi23 <piekielny7891>
2020-03-21 00:06:51 +0100

    sh

---
+++ get_zawada_lectures.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Created by Albert Piekielny
+#$1 link to presentation
+#$2 output director
+# ex : ./get_zawada_lectures.sh https://cs.pwr.edu.pl/zawada/kwjp/download/20200313/#/ ./wyk3/
+
+
+if [[ $# -lt 2 ]]; then
+	echo "Missing link to scaning '$1' or output directory '$2'";
+	exit 1;
+fi
+
+login=""					#your login to Zawada page
+password=""				# -- || -- password
+
+if [[ ${#login} -lt 6 || ${#password} -lt 1 ]]; then
+	echo "Please assign correct login and password to variable '$2'";
+	exit 1;
+fi
+	
+auth=`echo -n "$login:$password" | base64`
+
+correctLink=${1%#*}
+wget --header="Authorization: Basic $auth" -c -O index.html $1 
+
+targets=($(grep -o -i '[[:alnum:]]*\.png' index.html))
+
+arraylength=${#targets[@]}
+for (( i=0; i<${arraylength}; i++ ));
+do
+   wget --header="Authorization: Basic $auth" -q -c -O "$2$i.png" "$correctLink${targets[$i]}"
+	 echo -e "\033[1m\e[34mDownloaded ${targets[$i]} as $i.png \e[39m"
+done
+
+rm index.html
