d1a88e85718d5afafcc2a0f8115f345f19470ba6
albi23 <piekielny7891>
2020-03-29 17:47:53 +0200

    style

--- get_zawada_lectures.sh
+++ get_zawada_lectures.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#Created by Albert Piekielny
-#$1 link to presentation
-#$2 output director
-# ex : ./get_zawada_lectures.sh https://cs.pwr.edu.pl/zawada/kwjp/download/20200313/#/ ./wyk3/				<- directory ends with "/"
-# or  ./get_zawada_lectures.sh https://cs.pwr.edu.pl/zawada/kwjp/download/20200313/#/ ./wyk3/ pdf     <- it automatic create pdf file
-# NOTE : if pdf is not created please go to /etc/ImageMagick-${your_version} and in file "policy.xml" comment line    <policy domain="coder" rights="none" pattern="PDF" />
-# comment line    <policy domain="coder" rights="none" pattern="PDF" />  like that  <!-- <policy domain="coder" rights="none" pattern="PDF" /> -->
-
-
 if [[ $# -lt 2 ]]; then
 	echo "Missing link to scaning '$1' or output directory '$2'";
 	exit 1;
